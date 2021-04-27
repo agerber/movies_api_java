152a08dceff41ec958f832d7c5699e07430c7c4b
Adam Gerber <adam.gerber@yahoo.com>
2021-04-27 12:02:45 -0500

    add db

--- MoviesFunction/src/main/java/edu/uchicago/gerber/movies/CreateMovieLambda.java
+++ MoviesFunction/src/main/java/edu/uchicago/gerber/movies/CreateMovieLambda.java
@@ -1,17 +1,35 @@
 package edu.uchicago.gerber.movies;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 
 public class CreateMovieLambda {
 
    private final ObjectMapper objectMapper = new ObjectMapper();
 
+   private final AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard().build();
+   private final DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
+
     public APIGatewayProxyResponseEvent handleRequest (APIGatewayProxyRequestEvent requestEvent) throws JsonProcessingException {
 
-        Movie movie = objectMapper.readValue(requestEvent.getBody(), Movie.class);
+      Table moviesTable = dynamoDB.getTable(System.getenv("MOVIES_TABLE"));
+
+      Movie movie = new Movie("84884efc-74788483ce-77834876", "Avatar", 2010);
+      Item item = new Item()
+              .withPrimaryKey("id", movie.getId())
+              .withString("title", movie.getTitle())
+              .withInt("year", movie.getYear());
+
+       moviesTable.putItem(item);
+
+
         return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(movie.toString());
 
     }
