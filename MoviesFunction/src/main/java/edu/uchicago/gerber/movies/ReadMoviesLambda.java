b4bd88a43901405fc8c4fbac628e0f91c6f7263f
Adam Gerber <adam.gerber@yahoo.com>
2021-04-27 12:19:48 -0500

    refactor ReadMoviesLambda with db

--- MoviesFunction/src/main/java/edu/uchicago/gerber/movies/ReadMoviesLambda.java
+++ MoviesFunction/src/main/java/edu/uchicago/gerber/movies/ReadMoviesLambda.java
@@ -1,20 +1,46 @@
 package edu.uchicago.gerber.movies;
 
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ReadMoviesLambda {
 
-   // private final ObjectMapper objectMapper = new ObjectMapper();
+   private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard().build();
 
     public APIGatewayProxyResponseEvent handleRequest (APIGatewayProxyRequestEvent requestEvent) throws JsonProcessingException {
 
+
+        ScanRequest scanRequest = new ScanRequest().withTableName(System.getenv("MOVIES_TABLE"));
+        ScanResult scanResult = amazonDynamoDB.scan(scanRequest);
+
+       List<Movie> movies = scanResult.getItems().stream()
+                //map transform item - movie
+                .map(item -> new Movie(
+                        item.get("id").getS(),
+                        item.get("title").getS(),
+                        Integer.parseInt(item.get("year").getN())
+                ))
+                .collect(Collectors.toList());
+                //collect to a List
+
         //simulate reading form db
-        Movie movie = new Movie("8676787787d-7384ef-347884787ebac", "American Beauty", 1999 );
-        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(movie.toString());
+//        Movie movie = new Movie("8676787787d-7384ef-347884787ebac", "American Beauty", 1999 );
+
+        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(objectMapper.writeValueAsString(movies));
 
     }
 }
